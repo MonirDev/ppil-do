41072490fa81b1ad3b097a23fb0827f6abf2fcba
Monir Hossain <mdmonir1hossain@gmail.com>
2023-09-16 22:30:30 +0600

    update asm api

--- lib/app/data/models/sales_with_date_asm_model.dart
+++ lib/app/data/models/sales_with_date_asm_model.dart
@@ -13,6 +13,19 @@ class SalesWithDateASMListModel {
 
 class OrderAsmModel {
   final int? id;
+  final String? remarks;
+  final String? notes;
+  final String? saleType;
+  final String? deliveryAddress;
+  final String? deliveryContact;
+  final String? personnelName;
+  final int? personnelId;
+  final int? finYearId;
+  final String? finYear;
+  final String? createBy;
+  final DateTime? createDate;
+  final DateTime? deliveryDate;
+  final String? paymentMode;
   final bool? isApprovedAsm;
   final DateTime? saleOrderDate;
   final int? customerId;
@@ -26,6 +39,19 @@ class OrderAsmModel {
 
   OrderAsmModel({
     this.id,
+    this.remarks,
+    this.notes,
+    this.saleType,
+    this.deliveryAddress,
+    this.deliveryContact,
+    this.personnelName,
+    this.personnelId,
+    this.finYearId,
+    this.finYear,
+    this.createBy,
+    this.createDate,
+    this.deliveryDate,
+    this.paymentMode,
     this.isApprovedAsm,
     this.saleOrderDate,
     this.customerId,
@@ -40,6 +66,23 @@ class OrderAsmModel {
 
   factory OrderAsmModel.fromJson(Map<String, dynamic> json) => OrderAsmModel(
         id: json["id"],
+        remarks: json["remarks"],
+        notes: json["notes"],
+        saleType: json["saleType"],
+        deliveryAddress: json["deliveryAddress"],
+        deliveryContact: json["deliveryContact"],
+        personnelName: json["personnelName"],
+        personnelId: json["personnelId"],
+        finYearId: json["finYearId"],
+        finYear: json["finYear"],
+        createBy: json["createBy"],
+        createDate: json["createDate"] == null
+            ? null
+            : DateTime.parse(json["createDate"]),
+        deliveryDate: json["deliveryDate"] == null
+            ? null
+            : DateTime.parse(json["deliveryDate"]),
+        paymentMode: json["paymentMode"],
         isApprovedAsm: json["isApprovedAsm"],
         saleOrderDate: json["saleOrderDate"] == null
             ? null
@@ -49,13 +92,26 @@ class OrderAsmModel {
         subCustomerId: json["subCustomerId"],
         subCustomerName: json["subCustomerName"],
         orderNumber: json["orderNumber"],
-        billAmount: json["billAmount"]?.toDouble(),
+        billAmount: json["billAmount"],
         salesPerson: json["salesPerson"],
         asm: json["asm"],
       );
 
   Map<String, dynamic> toJson() => {
         "id": id,
+        "remarks": remarks,
+        "notes": notes,
+        "saleType": saleType,
+        "deliveryAddress": deliveryAddress,
+        "deliveryContact": deliveryContact,
+        "personnelName": personnelName,
+        "personnelId": personnelId,
+        "finYearId": finYearId,
+        "finYear": finYear,
+        "createBy": createBy,
+        "createDate": createDate?.toIso8601String(),
+        "deliveryDate": deliveryDate?.toIso8601String(),
+        "paymentMode": paymentMode,
         "isApprovedAsm": isApprovedAsm,
         "saleOrderDate": saleOrderDate?.toIso8601String(),
         "customerId": customerId,
@@ -65,6 +121,6 @@ class OrderAsmModel {
         "orderNumber": orderNumber,
         "billAmount": billAmount,
         "salesPerson": salesPerson,
-        "asm": asm
+        "asm": asm,
       };
 }
